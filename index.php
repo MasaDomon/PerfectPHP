f7d2d9eb079f2fcec46cf422a027d204ba5f58b2
MDomon <masamichi.d.engineer>
2018-09-12 07:47:11 +0900

    フォーム

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: masamichidomon
- * Date: 2018/09/09
- * Time: 12:49
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html"; charset="utf-8" />
+    <title>フォーム</title>
+</head>
+<body>
+    <?php if (isset($_GET["name"]) && strlen($_GET["name"]) > 0): ?>
 
+        <p><?php echo htmlspecialchars($_GET["name"], ENT_QUOTES, "UTF-8"); ?> さんこんにちは</p>
 
-echo "Hello World!";
+    <?php endif; ?>
+
+    <form action="index.php" method="get">
+        <p>名前を入力して下さい：
+            <input type="text" name="name" />
+            <input type="submit" value="送信" />
+        </p>
+    </form>
+</body>
+</html>
